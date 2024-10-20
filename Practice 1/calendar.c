2c5289c76473f305f4d0b11048679e7b461b82bf
tottfiy <ivanko.lisenko@gmail.com>
2024-10-20 03:15:21 +0300

    Fixed calendar.c

--- Practice 1/calendar.c
+++ Practice 1/calendar.c
@@ -135,7 +135,7 @@ int main()
 {
     int year = year_scan();
     int month = month_scan();
-    if (month > 12| month < 1){
+    if (month > 12| month < 1) {
         printf("Wrong input.\nWrite number of a month from 1 to 12.");
         return 0;
     } else{
