725479fdc0c0ae17302646ebde6480f13a6254cd
tottfiy <ivanko.lisenko@gmail.com>
2024-10-25 04:14:22 +0300

    Added Practice 6

---
+++ Practice 6/Task 2 (Additional)/array.h
@@ -0,0 +1,8 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+void print_array(int arr[10][10]);
+void change_array(int arr[10][10]);
+
+#endif
+
---
+++ Practice 6/Task 2 (Additional)/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "array.h"
+
+int main() {
+    printf("\n");  
+    int arr[10][10];
+    srand(time(NULL));
+    for (int i = 0; i<10; ++i) {
+        for (int j = 0; j<10; ++j) {
+            *(*(arr + i) + j) = rand() % 11;
+        }
+    } 
+    printf("Array before changes: \n\n");
+    print_array(arr);
+    change_array(arr);
+    printf("Array after changes: \n\n");
+    print_array(arr);
+    return 0;
+}
